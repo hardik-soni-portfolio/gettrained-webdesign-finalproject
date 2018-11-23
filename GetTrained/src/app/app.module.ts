987edf562ffb007efd7e7c53ac9418cfa83c30c0
Aman Gupta <gupta.am@husky.neu.edu>
2018-11-23 14:55:19 -0500

    Initialised the server skeleton
    Skeleton for server api for app

--- GetTrained/src/app/app.module.ts
+++ GetTrained/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
