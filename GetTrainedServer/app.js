d7ea5a1b9deaf69ee97d219f9a0b467a48469f79
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2018-11-28 02:02:01 -0500

    Created a Schema Model for query object on server side

--- GetTrainedServer/app.js
+++ GetTrainedServer/app.js
@@ -1,7 +1,10 @@
 module.exports = function(app){
     //enabling creation of schema
     let userModel = require('./models/user.model');
+    let queryModel = require('./models/query.model');
     //providing routes context for our application
     let userRoutes = require('./routes/user.route');
+    let queryRoutes = require('./routes/query.route');
     userRoutes(app);
+    queryRoutes(app);
 }
---
+++ GetTrainedServer/models/query.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/GetTrained', (err) => {
+    if(!err){ console.log('MongoDB connection succeeded.'); }
+    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+});
+
+// const bcrypt = require('bcryptjs');
+
+var querySchema = new mongoose.Schema({
+    query_title:{
+        type: String,
+        required: 'Query title cannot be empty'
+    },
+    query_content: {
+        type: String,
+        required: 'Query content cannot be empty'
+    },
+    query_type: {
+        type: String,
+        required: 'Query type cannot be empty'
+    }
+});
+
+
+mongoose.model('Queries', querySchema);
