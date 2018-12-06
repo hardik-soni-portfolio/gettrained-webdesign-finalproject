6616927d4d462a479d23aa8e67ad7020ef0d4dbf
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2018-12-06 15:34:46 -0500

    Removing node_modules

--- GetTrainedServer/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    const userController = require('../controllers/user.controller'),
-            emailController = require('../controllers/email.controller');
-
-    app.route('/register').post(userController.register); //invoke register function on /register route
-    app.route('/activate/:token').put(emailController.activate); //invoke activate function on /activate/:token route
-    app.route('/login').post(userController.login);
-};
