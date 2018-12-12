88405f8fc62b9b7b3055c8a05847f008fe42b2c7
Hardik Bhupendra Soni <43351568+ihardiks@users.noreply.github.com>
2018-12-12 11:16:05 -0500

    Image uploading completed

--- GetTrained/src/app/services/category.service.ts
+++ GetTrained/src/app/services/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -7,12 +7,13 @@ import { environment } from 'src/environments/environment';
 })
 export class CategoryService {
 
-  constructor(private http:HttpClient) {}
-
-  getCategories(){
+  constructor(private http: HttpClient) { }
+  //sends request to server to fetch categories
+  getCategories() {
     return this.http.get(`${environment.apiBaseUrl}/categories`);
   }
 
+  //sends request to server to add a new category
   addCategory(category_name) {
     const category = {
       category_name: category_name
@@ -20,6 +21,7 @@ export class CategoryService {
     return this.http.post(`${environment.apiBaseUrl}/categories`, category);
   }
 
+  //sends request to server to delete added category
   deleteCategory(id) {
     return this.http.delete(`${environment.apiBaseUrl}/categories/${id}`);
   }
